f950ddbb3474fbf9775974004c8809c6538c8c67
sunzhenkai <sunzhenkai@xiaomi.com>
2019-09-17 07:45:14 -0700

    fix add softeare

--- install-soft.sh
+++ install-soft.sh
@@ -2,8 +2,16 @@
 
 set -x
 
+mvToNext() {
+	n=1
+	while [ -e "${1}.back${n}" ]; do
+	    n=$((n+1))
+	done
+	mv "$1" "$1.back${n}"
+}
+
 [ ! -e software ] && echo "software dirctory not exists" && exit 1
-[ -e ~/.software ] && echo "~/.software already exists" && mv ~/.software ~/.software-back
+[ -e ~/.software ] && echo "~/.software already exists" && mvToNext ~/.software
 
 cp -r software ~/.software
 
--- software/install.sh
+++ software/install.sh
@@ -10,9 +10,17 @@ appendIfNotExsits() {
     (! grep -q "$2" $1)  && echo "[ $2 ] not exists" && echo "$2" >> $1
 }
 
+mvToNext() {
+	n=1
+	while [ -e "${1}.back${n}" ]; do
+	    n=$((n+1))
+	done
+	mv "$1" "$1.back${n}"
+}
+
 appendIfNotExsits ${bash_profile} "alias vim='mvim -v'"
 appendIfNotExsits ${bash_profile} "alias ll='ls -lGh'"
-appendIfNotExsits ${bash_profile} "export PATH=$software_dir/thrift-0.5.0/bin:$PATH"
-appendIfNotExsits ${bash_profile} "export PATH=$software_dir/protoc-3.5.1-osx-x86_64/bin:$PATH"
+appendIfNotExsits ${bash_profile} "export PATH=$software_dir/thrift-0.5.0/bin:\$PATH"
+appendIfNotExsits ${bash_profile} "export PATH=$software_dir/protoc-3.5.1-osx-x86_64/bin:\$PATH"
 
 
