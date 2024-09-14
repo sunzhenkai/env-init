9db773558ef354470ea4942409cf847c66f697a5
Wii <zhenkai.sun@qq.com>
2024-09-14 14:26:21 +0800

    Add/miniconda (#11)

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,33 +5,35 @@ source "$_BASE/scripts/tool.sh"
 # export Variables
 # 需要设置
 function ExportEnvs() {
-    [ "$NEW_HOME" = "" ] && export NEW_HOME="$HOME"
-    export ENV_INIT_DIR="$NEW_HOME/.env-init"
+    [ -z "$NEW_HOME" ] && export NEW_HOME="$HOME"
+    export ENV_INIT_DIR="$NEW_HOME/.local/env-init"
     export ENV_INIT_ENV_FILE="$ENV_INIT_DIR/env"
     export ENV_INSTALL_DIR="$ENV_INIT_DIR/app"
     export ENV_INSTALL_PACKAGE_DIR="$ENV_INIT_DIR/packages"
-    export PATH="$_BASE/bin":$PATH
     export ENV_INIT_COMMAND=ienv
-    export ENV_INIT_CODE=$_BASE
+    export ENV_INIT_BASE="$_BASE"
+    export ENV_INIT_BIN="$_BASE/bin"
+    export PATH="$ENV_INIT_BIN":$PATH
 }
 
 # init env file
 function EnvInit() {
+    mkdir -p "${ENV_INIT_BIN}"
     mkdir -p "${ENV_INSTALL_DIR}"
     mkdir -p "${ENV_INSTALL_PACKAGE_DIR}"
-    
+
     # init env file
     tool::append_to_env_profile "export HOME=\"$NEW_HOME\""
     tool::append_to_env_profile 'export ENV_INIT_DIR="$HOME/.env-init"'
     tool::append_to_env_profile 'export ENV_INIT_ENV_FILE="$ENV_INIT_DIR/env"'
     tool::append_to_env_profile 'export ENV_INSTALL_DIR="$ENV_INIT_DIR/app"'
     tool::append_to_env_profile 'export ENV_INSTALL_PACKAGE_DIR="$ENV_INIT_DIR/packages"'
-    tool::append_to_env_profile "export ENV_INIT_CODE=$_BASE"
+    tool::append_to_env_profile "export ENV_INIT_BASE=$_BASE"
     tool::append_to_env_profile "alias envi=\"cd $_BASE\""
     tool::append_to_env_profile "export PATH=$_BASE/bin:\$PATH"
 
-    tool::append_to_env_profile_lines < "$_BASE"/env/profile
-    tool::append_to_env_profile_lines < "$_BASE/env/profile_$(tool::os_type)"
+    tool::append_to_env_profile_lines <"$_BASE"/env/profile
+    tool::append_to_env_profile_lines <"$_BASE/env/profile_$(tool::os_type)"
     # [ -e "$_BASE/env/profile_$(tool::os_type)" ] && tool::append_to_env_profile_lines < "$_BASE/env/profile_$(tool::os_type)"
 }
 
@@ -46,4 +48,3 @@ function InitEnvCommand {
 function InitEnvSource {
     tool::append_to_profiles "source \"${ENV_INIT_ENV_FILE}\""
 }
-
