3b67fbac82e2081bb0ee3e106311af6f70364c05
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-05 14:57:52 +0800

    add zsh

---
+++ app/zsh/gate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# basic check
+_BASE=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+source "${_BASE}/../../scripts/tool.sh"
+[ ! tool::pre_install_check ] && exit 1
+
+# basic information
+
+# functions
+function usage() {
+  cat <<EOF
+Usage: ii $APP [args]
+
+Args
+    -c  config
+EOF
+}
+
+function install() {
+}
+
+function config() {
+  # install oh my zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+TASK="usage"
+while getopts ":cv:" opt; do
+  case $opt in
+  c) TASK="config" ;;
+  v) VERSION=$OPTARG ;;
+  *) usage ;;
+  esac
+done
+
+$TASK
