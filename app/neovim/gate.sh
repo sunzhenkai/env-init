aad06dd7a9b7993db449783d37e6d3529ec01ba9
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-13 17:12:36 +0800

    update

--- app/neovim/gate.sh
+++ app/neovim/gate.sh
@@ -20,16 +20,17 @@ EOF
 }
 
 function install() {
-  DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/v${VERSION}/nvim-linux64.tar.gz"
-  tool::download $DOWNLOAD_URL $APP $VERSION
-  tool::tar_extract $APP $VERSION install
-  tool::update_install_link $APP $VERSION
-  tool::append_binary_path $APP bin
+  brew install neovim
+  # DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/v${VERSION}/nvim-linux64.tar.gz"
+  # tool::download $DOWNLOAD_URL $APP $VERSION
+  # tool::tar_extract $APP $VERSION install
+  # tool::update_install_link $APP $VERSION
+  # tool::append_binary_path $APP bin
 }
 
 function config() {
-  [[ -e "$HOME/.config/nvim" ]] && mv "$HOME/.config/nvim" "$HOME/.config/nvim-$(date +%s)"
-  git clone --recursive https://github.com/sunzhenkai/nvim.git "$HOME/.config/nvim"
+  # [[ -e "$HOME/.config/nvim" ]] && mv "$HOME/.config/nvim" "$HOME/.config/nvim-$(date +%s)"
+  # git clone --recursive https://github.com/sunzhenkai/nvim.git "$HOME/.config/nvim"
 }
 
 TASK="install"
--- app/ubuntu/gate.sh
+++ app/ubuntu/gate.sh
@@ -38,36 +38,15 @@ function config() {
 
   # zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  # homebrew
-  if ! command -v brew; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo >>$HOME/.zshrc
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>$HOME/.zshrc
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  fi
   brew install openjdk@17 cmake ninja bison flex htop nvim go ripgrep
   brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide imagemagick font-symbols-only-nerd-font
   brew install fish starship lazygit gitui
-  echo "" >>~/.zshrc
-  echo 'eval "$(starship init zsh)"' >>~/.zshrc
 
-  # nvm
-  if ! command -v nvm; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-    echo '' >>~/.zshrc
-    echo 'export NVM_DIR="$HOME/.nvm"' >>~/.zshrc
-    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >>~/.zshrc
-    echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' >>~/.zshrc
-    source ~/.zshrc
-  fi
   nvm install --lts
   npm install -g yarn
 
   # env
   pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-  echo "" >>~/.zshrc
-  echo "export CC=/usr/bin/gcc-$GCC_VERSION" >>~/.zshrc
-  echo "export CXX=/usr/bin/g++-$GCC_VERSION" >>~/.zshrc
 }
 
 TASK="install"
