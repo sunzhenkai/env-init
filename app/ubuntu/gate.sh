7da2cddebf19bf17d64448df0f568517138bbec5
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-18 15:29:37 +0800

    update

--- app/ubuntu/gate.sh
+++ app/ubuntu/gate.sh
@@ -32,7 +32,7 @@ function config() {
   sudo apt install -y gcc-$GCC_VERSION g++-$GCC_VERSION gettext grep gzip libtool m4 make patch pkgconf sed texinfo
   sudo apt install -y python3-pip python3-virtualenv python3-venv
   sudo apt install -y gdb wget curl vim zip unzip tar xz-utils
-  sudo apt install -y build-essential procps patch
+  sudo apt install -y build-essential procps patch sqlite3
   sudo apt install -y imagemagick libmagickwand-dev
   sudo ln -fs /usr/bin/gcc-$GCC_VERSION /usr/bin/gcc
   sudo ln -fs /usr/bin/g++-$GCC_VERSION /usr/bin/g++
