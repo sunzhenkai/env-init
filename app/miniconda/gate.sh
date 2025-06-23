73d090cfdaa20e738279f3d5f8f38c5b910033d7
zhenkai.sun <zhenkai.sun@qq.com>
2025-06-23 16:29:35 +0800

    fix miniconda

--- app/miniconda/gate.sh
+++ app/miniconda/gate.sh
@@ -9,7 +9,7 @@ source "${_BASE}/../../scripts/tool.sh"
 
 # functions
 function usage() {
-    cat <<EOF
+  cat <<EOF
 Usage: ii miniconda [args]
 
 Args
@@ -19,52 +19,51 @@ EOF
 }
 
 function config() {
-    export CONDA_ALWAYS_YES="true"
-    [[ -z "$ENV_NAME" ]] && FATAL "empty ENV_NAME"
+  export CONDA_ALWAYS_YES="true"
+  [[ -z "$ENV_NAME" ]] && FATAL "empty ENV_NAME"
 
-    local INSTALL_DIR=$(tool::get_install_dir $APP $VERSION)
-    if [[ -e "$INSTALL_DIR" ]]; then
-        ${INSTALL_DIR}/bin/conda create -n $ENV_NAME --channel=conda-forge
-    else
-        conda create -n $ENV_NAME --channel=conda-forge
-    fi
-    tool::append_to_env_profile "conda activate $ENV_NAME"
+  local INSTALL_DIR=$(tool::get_install_dir $APP $VERSION)
+  if [[ -e "$INSTALL_DIR" ]]; then
+    ${INSTALL_DIR}/bin/conda create -n $ENV_NAME --channel=conda-forge
+  else
+    conda create -n $ENV_NAME --channel=conda-forge
+  fi
+  tool::append_to_env_profile "conda activate $ENV_NAME"
 }
 
 function install() {
-    $(tool::is_installed $APP $VERSION) && FATAL "already installed"
-    export CONDA_ALWAYS_YES="true"
-    local DOWNLOAD_URL=https://repo.anaconda.com/miniconda/Miniconda3-${VERSION}-Linux-x86_64.sh
-    local DOWNLOAD_FILE=$(tool::get_download_file $APP $VERSION)
-    local INSTALL_DIR=$(tool::get_install_dir $APP $VERSION)
-    tool::download $DOWNLOAD_URL $APP $VERSION
-    ln -s "${DOWNLOAD_FILE}" "${DOWNLOAD_FILE}".sh
-    mkdir -p "${INSTALL_DIR}"
-    bash "${DOWNLOAD_FILE}".sh -b -u -p "${INSTALL_DIR}"
-    ${DOWNLOAD_FILE}
+  $(tool::is_installed $APP $VERSION) && FATAL "already installed"
+  export CONDA_ALWAYS_YES="true"
+  local DOWNLOAD_URL=https://repo.anaconda.com/miniconda/Miniconda3-${VERSION}-Linux-x86_64.sh
+  local DOWNLOAD_FILE=$(tool::get_download_file $APP $VERSION)
+  local INSTALL_DIR=$(tool::get_install_dir $APP $VERSION)
+  tool::download $DOWNLOAD_URL $APP $VERSION
+  ln -s "${DOWNLOAD_FILE}" "${DOWNLOAD_FILE}".sh
+  mkdir -p "${INSTALL_DIR}"
+  bash "${DOWNLOAD_FILE}".sh -b -u -p "${INSTALL_DIR}"
 
-    command -v bash >/dev/null && ${INSTALL_DIR}/bin/conda init bash
-    command -v zsh >/dev/null && ${INSTALL_DIR}/bin/conda init zsh
+  command -v bash >/dev/null && ${INSTALL_DIR}/bin/conda init bash
+  command -v zsh >/dev/null && ${INSTALL_DIR}/bin/conda init zsh
 
-    tool::append_binary_path $APP bin
-    [[ -n "$ENV_NAME" ]] && config
+  tool::append_binary_path $APP bin
+  [[ -n "$ENV_NAME" ]] && config
 }
 
 function build() {
-    echo "[$APP] Build is not support [version=$VERSION]"
-    exit 1
+  echo "[$APP] Build is not support [version=$VERSION]"
+  exit 1
 }
 
 TASK="usage"
 VERSION=latest
 while getopts ":ib:e:v:" opt; do
-    case $opt in
-    i) TASK="install" ;;
-    b) TASK="build" ;;
-    e) ENV_NAME=$OPTARG ;;
-    v) VERSION=$OPTARG ;;
-    *) usage ;;
-    esac
+  case $opt in
+  i) TASK="install" ;;
+  b) TASK="build" ;;
+  e) ENV_NAME=$OPTARG ;;
+  v) VERSION=$OPTARG ;;
+  *) usage ;;
+  esac
 done
 
 $TASK
