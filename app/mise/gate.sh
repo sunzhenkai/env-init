bdd99247e0cb63c72af0afb7882d89550f691372
zhenkai.sun <zhenkai.sun@qq.com>
2026-01-31 18:29:16 +0800

    mod mise

--- app/mise/gate.sh
+++ app/mise/gate.sh
@@ -6,6 +6,14 @@ source "${_BASE}/../../scripts/tool.sh"
 
 # basic information
 
+# Define applications and their versions
+declare -a MISE_APPS=(
+  "golang:1.24.9"
+  "python:3.13.3"
+  "cmake:3.31.7"
+  "nodejs:latest"
+)
+
 # functions
 function usage() {
   cat <<EOF
@@ -14,6 +22,7 @@ Usage: ii $APP [args]
 Args
     -i  install
     -c  config
+    -u  uninstall
 EOF
 }
 
@@ -28,21 +37,48 @@ function install_app() {
   mise use -g $1@$2
 }
 
+function uninstall_app() {
+  if mise list | grep -q "^$1"; then
+    echo "Uninstalling $1..."
+    mise uninstall $1@$2
+    echo "$1 uninstalled successfully!"
+  else
+    echo "$1 is not installed via mise"
+  fi
+}
+
 function config() {
   echo "Configuring mise applications..."
-  install_app golang 1.24.9
-  # install_app zellij latest
-  install_app python 3.13.3
-  install_app cmake 3.31.7
-  install_app nodejs latest
+  for app_info in "${MISE_APPS[@]}"; do
+    app=$(echo $app_info | cut -d':' -f1)
+    version=$(echo $app_info | cut -d':' -f2)
+    install_app $app $version
+  done
   echo "mise configuration completed!"
 }
 
+function uninstall() {
+  echo "Uninstalling mise and its installed applications..."
+  if command -v mise >/dev/null; then
+    for app_info in "${MISE_APPS[@]}"; do
+      app=$(echo $app_info | cut -d':' -f1)
+      version=$(echo $app_info | cut -d':' -f2)
+      echo "Uninstalling $app..."
+      uninstall_app $app $version
+    done
+    brew uninstall mise
+    echo "mise and its applications uninstalled successfully!"
+  else
+    echo "mise is not installed"
+  fi
+}
+
 TASK="install"
-while getopts ":icv:" opt; do
+while getopts ":icuv:" opt; do
   case $opt in
   i) TASK="install" ;;
   c) TASK="config" ;;
+  u) TASK="uninstall" ;;
   v) VERSION=$OPTARG ;;
   *) usage ;;
   esac
